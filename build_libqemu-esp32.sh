31e053715498a4066167ad2e441330ad8daa9d77
ishwarmudraje <ishwar.mudraje@uni-saarland.de>
2024-08-13 16:03:11 +0200

    Changes to build scripts for compilation on arch

--- build_libqemu-esp32.sh
+++ build_libqemu-esp32.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-target="xtensa-softmmu,riscv32-softmmu"
+target="riscv32-softmmu"
 
 case "$(uname)" in
   Linux)
@@ -26,30 +26,33 @@ make clean >/dev/null
 # Build everything as usual
 make "-j$ncpu" 
 
+rm -r ../qemu-esp32
+mv build ../qemu-esp32
+
+
 # Build a shared library, without softmmu/main.o and otherwise *exactly* the same
 # flags.
-cd build
-rm -f qemu-system-xtensa
-ninja -v > qemu-system-xtensa.rsp
-sed -i -n '$p' qemu-system-xtensa.rsp
-CMD=$(sed  's/-o .*//' qemu-system-xtensa.rsp | sed 's/\[.\/.\] //g')
-sed -i 's/.*-o /-o /' qemu-system-xtensa.rsp
+# cd build
+# rm -f qemu-system-xtensa
+# ninja -v > qemu-system-xtensa.rsp
+# sed -i -n '$p' qemu-system-xtensa.rsp
+# CMD=$(sed  's/-o .*//' qemu-system-xtensa.rsp | sed 's/\[.\/.\] //g')
+# sed -i 's/.*-o /-o /' qemu-system-xtensa.rsp
 
-#dynamic
-sed -i 's/qemu-system-xtensa.p\/softmmu_main.c.o//g' qemu-system-xtensa.rsp
-sed -i 's/-o\ qemu-system-xtensa/-shared\ -o\ libqemu-xtensa.so/g' qemu-system-xtensa.rsp
-eval "$CMD -ggdb @qemu-system-xtensa.rsp"
-
-
-rm -f qemu-system-riscv32
-ninja -v > qemu-system-riscv32.rsp
-sed -i -n '$p' qemu-system-riscv32.rsp
-CMD=$(sed  's/-o .*//' qemu-system-riscv32.rsp | sed 's/\[.\/.\] //g')
-sed -i 's/.*-o /-o /' qemu-system-riscv32.rsp
+# #dynamic
+# sed -i 's/qemu-system-xtensa.p\/softmmu_main.c.o//g' qemu-system-xtensa.rsp
+# sed -i 's/-o\ qemu-system-xtensa/-shared\ -o\ libqemu-xtensa.so/g' qemu-system-xtensa.rsp
+# eval "$CMD -ggdb @qemu-system-xtensa.rsp"
 
 
-#dynamic
-sed -i 's/qemu-system-riscv32.p\/softmmu_main.c.o//g' qemu-system-riscv32.rsp
-sed -i 's/-o\ qemu-system-riscv32/-shared\ -o\ libqemu-riscv32.so/g' qemu-system-riscv32.rsp
-eval "$CMD -ggdb @qemu-system-riscv32.rsp"
+# rm -f qemu-system-riscv32
+# ninja -v > qemu-system-riscv32.rsp
+# sed -i -n '$p' qemu-system-riscv32.rsp
+# CMD=$(sed  's/-o .*//' qemu-system-riscv32.rsp | sed 's/\[.\/.\] //g')
+# sed -i 's/.*-o /-o /' qemu-system-riscv32.rsp
 
+
+# #dynamic
+# sed -i 's/qemu-system-riscv32.p\/softmmu_main.c.o//g' qemu-system-riscv32.rsp
+# sed -i 's/-o\ qemu-system-riscv32/-shared\ -o\ libqemu-riscv32.so/g' qemu-system-riscv32.rsp
+# # eval "$CMD -ggdb @qemu-system-riscv32.rsp"
